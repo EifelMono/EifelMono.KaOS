ad94c891414d91012526e6f83a112b5150d79a94
Andreas Klapperich <andreas@klapperich.de>
2017-04-16 13:29:52 +0200

    Test Console project added

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EifelMono.KaOS.Extensions;
+
+namespace ConsoleTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            "Hallo".LogInfo();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
